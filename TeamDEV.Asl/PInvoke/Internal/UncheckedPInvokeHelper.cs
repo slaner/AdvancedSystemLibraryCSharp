d1cd92e9aaecd5a6b7b0249877d03703af2ae6b9
slaner <black_scv@naver.com>
2018-11-06 01:01:16 +0900

    Add NtTerminateProcess support/add NtClose method in UncheckedPInvokeHelper class

--- TeamDEV.Asl/PInvoke/Internal/UncheckedPInvokeHelper.cs
+++ TeamDEV.Asl/PInvoke/Internal/UncheckedPInvokeHelper.cs
@@ -2,32 +2,17 @@
 
 using TeamDEV.Asl.PInvoke.Enumerations;
 using TeamDEV.Asl.PInvoke.Internal.Methods;
-using TeamDEV.Asl.PInvoke.Structures;
 
 namespace TeamDEV.Asl.PInvoke.Internal {
-    static class UncheckedPInvokeHelper {
-        public static IntPtr OpenProcess(ProcessAccess access, bool inherit, int pid) {
-            return Kernel32.PInvoke_OpenProcess(access, inherit, pid);
+    static partial class UncheckedPInvokeHelper {
+        public static ProcessAccess GetLimitedQueryAccess() {
+            return Environment.OSVersion.Version.Major >= 6 ? ProcessAccess.QueryLimitedInformation : ProcessAccess.QueryInformation;
         }
-        public static IntPtr OpenProcessNative(ProcessAccess access, bool inherit, int pid) {
-            IntPtr hProcess = IntPtr.Zero;
-            ObjectAttributes objectAttributes = default(ObjectAttributes);
-            objectAttributes.Attributes = inherit ? ObjectAttributesFlags.Inherit : 0;
-            objectAttributes.Length = (uint) ObjectAttributes.Size;
-            ClientId clientId = default(ClientId);
-            clientId.UniqueProcess = new IntPtr(pid);
-
-            NTSTATUS result = Ntdll.PInvoke_NtOpenProcess(ref hProcess, access, ref objectAttributes, ref clientId);
-            if (result.IsSuccess())
-                return hProcess;
-            return IntPtr.Zero;
+        public static bool NtClose(IntPtr hObject) {
+            return Ntdll.PInvoke_NtClose(hObject).IsSuccess();
         }
-
-        public static bool TerminateProcess(IntPtr processHandle, int exitCode) {
-            return Kernel32.PInvoke_TerminateProcess(processHandle, exitCode);
-        }
-        public static bool TerminateProcessNative(IntPtr processHandle, NTSTATUS exitStatus) {
-            return Ntdll.PInvoke_NtTerminateProcess(processHandle, exitStatus).IsSuccess();
+        public static bool NtCloseIfTrue(IntPtr hObject, bool statement) {
+            return statement ? NtClose(hObject) : statement;
         }
     }
 }
