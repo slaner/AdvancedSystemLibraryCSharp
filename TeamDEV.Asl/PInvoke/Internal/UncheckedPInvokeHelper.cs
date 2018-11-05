b68c9229c16fcfc181725998d36be0ff973a9912
slaner <black_scv@naver.com>
2018-11-05 20:31:30 +0900

    Add NtStatusResolver, UncheckedPInvokeHelper and ObjectAttributesFlags enumeration.

---
+++ TeamDEV.Asl/PInvoke/Internal/UncheckedPInvokeHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using TeamDEV.Asl.PInvoke.Enumerations;
+using TeamDEV.Asl.PInvoke.Internal.Methods;
+using TeamDEV.Asl.PInvoke.Structures;
+
+namespace TeamDEV.Asl.PInvoke.Internal {
+    static class UncheckedPInvokeHelper {
+        public static IntPtr OpenProcess(ProcessAccess access, bool inherit, int pid) {
+            return Kernel32.PInvoke_OpenProcess(access, inherit, pid);
+        }
+        public static IntPtr OpenProcessNative(ProcessAccess access, bool inherit, int pid) {
+            IntPtr hProcess = IntPtr.Zero;
+            ObjectAttributes objectAttributes = default(ObjectAttributes);
+            objectAttributes.Attributes = inherit ? ObjectAttributesFlags.Inherit : 0;
+            objectAttributes.Length = (uint) ObjectAttributes.Size;
+            ClientId clientId = default(ClientId);
+            clientId.UniqueProcess = new IntPtr(pid);
+
+            NTSTATUS result = Ntdll.PInvoke_NtOpenProcess(ref hProcess, access, ref objectAttributes, ref clientId);
+            if (result.IsSuccess())
+                return hProcess;
+            return IntPtr.Zero;
+        }
+
+        public static bool TerminateProcess(IntPtr processHandle, int exitCode) {
+            return Kernel32.PInvoke_TerminateProcess(processHandle, exitCode);
+        }
+        public static bool TerminateProcessNative(IntPtr processHandle, NTSTATUS exitStatus) {
+            return Ntdll.PInvoke_NtTerminateProcess(processHandle, exitStatus).IsSuccess();
+        }
+    }
+}
---
+++ TeamDEV.Asl/PInvoke/NtStatusResolver.cs
@@ -0,0 +1,43 @@
+﻿using TeamDEV.Asl.PInvoke.Enumerations;
+
+namespace TeamDEV.Asl.PInvoke {
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class NtStatusResolver {
+        public NtStatusResolver(NTSTATUS status) {
+            uint statusValue = (uint) status;
+            ushort high = (ushort) ((statusValue & 0xFFFF0000U) >> 16);
+
+            SeverityCode = (SeverityCode) ((high & 0xC000) >> 14);
+            CustomerCode = (byte) ((high & 0x2000) >> 13);
+            FacilityCode = (FacilityCode) (high & 0x1FFF);
+            StatusCode = (ushort) (statusValue & 0xFFFF);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SeverityCode SeverityCode { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte CustomerCode { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public FacilityCode FacilityCode { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public ushort StatusCode { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() {
+            return $"Severity: {SeverityCode}, Facility: {FacilityCode}, Status: {StatusCode}";
+        }
+    }
+}
