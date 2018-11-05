83592e7029ae91226029a936e4a7b6c1d7bb1e29
slaner <black_scv@naver.com>
2018-11-06 01:16:58 +0900

    Add IntPtr extension methods / building Process class

--- TeamDEV.Asl/SystemManagements/Process/Process.cs
+++ TeamDEV.Asl/SystemManagements/Process/Process.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamDEV.Asl.PInvoke.Enumerations;
+using TeamDEV.Asl.PInvoke.Internal;
 
 namespace TeamDEV.Asl.SystemManagements.Process {
     /// <summary>
     /// 
     /// </summary>
-    public sealed class Process: SystemObject {
-        public Process(): base(0) {
+    public sealed class Process: IIdentifiable, ITerminatable {
+        public static Process Invalid { get; }
+        static Process() { Invalid = new Process();  }
+        public Process() : this(0) { }
+        public Process(int id) {
+            Id = id;
+        }
 
+
+
+
+
+        public int Id { get; }
+        public bool IsTerminated { get; private set; }
+
+        public bool Terminate() {
+            return Terminate(0);
+        }
+        public bool Terminate(int exitCode) {
+            // TODO: Implement process termination
+            if (IsTerminated) return false;
+            return false;
         }
     }
 }
