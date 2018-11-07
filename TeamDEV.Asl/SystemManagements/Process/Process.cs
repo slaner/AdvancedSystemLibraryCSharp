d15ba608a0dc828a4644896fb4e13b8654f17dbf
SlaneR <black_scv@naver.com>
2018-11-08 00:32:21 +0900

    Change ITerminatable method signature, add IsValid property on Process class

--- TeamDEV.Asl/SystemManagements/Process/Process.cs
+++ TeamDEV.Asl/SystemManagements/Process/Process.cs
@@ -13,7 +13,7 @@ namespace TeamDEV.Asl.SystemManagements.Process {
     public sealed class Process: IIdentifiable, ITerminatable {
         public static Process Invalid { get; }
         static Process() { Invalid = new Process();  }
-        public Process() : this(0) { }
+        public Process() : this(-1) { }
         public Process(int id) {
             Id = id;
         }
@@ -24,13 +24,15 @@ namespace TeamDEV.Asl.SystemManagements.Process {
 
         public int Id { get; }
         public bool IsTerminated { get; private set; }
+        public bool IsValid => Id >= 0;
 
         public bool Terminate() {
             return Terminate(0);
         }
         public bool Terminate(int exitCode) {
             // TODO: Implement process termination
-            if (IsTerminated) return false;
+            if (!IsValid || IsTerminated) return false;
+
             return false;
         }
     }
